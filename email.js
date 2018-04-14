ed9e920c5b587ac6a016d7f0c191ff3633358369
Nick <njgrout@gmail.com>
2018-04-13 17:23:59 -0700

    Added email script

---
+++ email.js
@@ -0,0 +1,25 @@
+var mailer = require('nodemailer');
+var fs = require('fs');
+var path = require('path');
+
+var auth = fs.readFileSync(path.join(__dirname,'email.json'));
+auth = JSON.parse(auth);
+console.log(auth);
+
+var transporter = mailer.createTransport({
+    service: 'gmail',
+    auth
+});
+
+var mailOptions = {
+    from: auth.user,
+    to: auth.user,
+    subject: 'test!',
+    text: 'That was easy!'
+};
+
+
+transporter.sendMail(mailOptions, (err, info) => {
+    if(err) console.log(err)
+    else console.log('Email send: ' + info.response);
+});
